90465a4d906afb47ed99209a01917cf8a8423210
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-01-29 14:34:56 -0500

    Fixing error code bash

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ git pull --recurse-submodules
 
 java -cp bin/ili2c.jar ch.interlis.ili2c.MakeIliModelsXml LADM_COL
 
-rm html/ilimodels.xml
+rm html/ilimodels.xml | true
 cp LADM_COL/ilimodels.xml html
 
-rm -r html/LADM_COL
+rm -r html/LADM_COL | true
 mkdir html/LADM_COL
 cp LADM_COL/*.ili html/LADM_COL
 
