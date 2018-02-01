1ba448d804eeb00903c41b7f6a7d041a6d49e8dd
Jorge Useche <naturalmentejorge@gmail.com>
2018-02-01 14:07:56 -0500

    Versión Feb 1, 2018 LADM_COL

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git submodule update --recursive --init
 java -cp bin/ili2c.jar ch.interlis.ili2c.MakeIliModelsXml LADM_COL
 
 rm html/ilimodels.xml | true
-cp LADM_COL/ilimodels.xml html
+mv LADM_COL/ilimodels.xml html
 
 rm -rf html/LADM_COL | true
 find LADM_COL/ -name '*.ili' -exec cp --parents \{\} html \;
